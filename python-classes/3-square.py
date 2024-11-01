e29fecfb7e6f2858e5741e3ef237ce0944955c60
62laura <c.ishimwe8@alustudent.com>
2024-11-01 02:59:30 -0700

    quick fix

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
 class Square:
 
     def __init__(self, size=0):
+        """
+        this initalises the class with size attribute ,thus validate the input
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -9,4 +15,7 @@ class Square:
         self.__size = size
 
     def area(self):
+        """
+        this calculates the area of the square
+        """
         return self.__size**2
