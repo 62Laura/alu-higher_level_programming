d2300e05cf38341604d170279aec3ff02178262f
62laura <c.ishimwe8@alustudent.com>
2024-11-04 02:39:44 -0800

    4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""createa a square"""
+
+class Square:
+    ''' Instatiates a class square '''
+def __init__(self,size=0):
+'''init size'''
+self.__size = size
+   @property
+    def size(self):
+        "returns the size att"
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        '''asign the size to the size att'''
+        if size != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+          def area(self):
+        square_area = self.__size ** 2
+        return square_area
