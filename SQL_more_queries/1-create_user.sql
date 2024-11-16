ea7e93738f63630a922c7a94a00ee7c32172f8bd
62laura <c.ishimwe8@alustudent.com>
2024-11-16 03:08:54 -0800

    1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
--- create use if not there 
+-- 1-create_user.sql: Create user user_0d_1 with full privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
