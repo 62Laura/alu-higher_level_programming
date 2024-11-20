396814d0a97cf27bc5a0c82443be094fa122084e
62laura <c.ishimwe8@alustudent.com>
2024-11-20 11:41:41 +0000

    bug fix

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter"""
+""script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter""
 
 import urllib.request
 import urllib.parse
