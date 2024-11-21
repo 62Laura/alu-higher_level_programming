f025c98d1265ccf9a35af40e1bfb3013b5004975
62laura <c.ishimwe8@alustudent.com>
2024-11-21 11:17:10 +0000

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""
-    This script makes a request and displays a
-    response using the requests package.
-"""
+"""Fetch a url"""
 
 import requests
 
-if __name__ == "__main__":
-    response = requests.get("https://alu-intranet.hbtn.io/status")
 
+if __name__ == "__main__":
+    response = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print(f"\t- type: {type(response.text)}")
-    print(f"\t- content: {response.text}")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
